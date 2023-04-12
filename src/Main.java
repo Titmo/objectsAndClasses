a23a2228c6ce986bb9c5a1e79ecf67265910988e
Danila <03denchik03@gmail.com>
2023-04-12 17:45:21 +0300

    Всё исправил(Финальный штрих)

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
         Book warAndPeace = new Book("Война и мир", authorWarAndPeace, 1867);
         Author authorCrimeAndPunishment = new Author("Ф.М.Достоевский");
         Book crimeAndPunishment = new Book("Преступление и наказание",authorCrimeAndPunishment,1866);
-        System.out.println(warAndPeace);
-        System.out.println(crimeAndPunishment);
+//        System.out.println(warAndPeace);
+//        System.out.println(crimeAndPunishment);
+        System.out.println(warAndPeace.getAuthor() + " " + warAndPeace.getBookName() + " " + warAndPeace.getYear());
+        System.out.println(crimeAndPunishment.getAuthor() + " " + crimeAndPunishment.getBookName() + " " + crimeAndPunishment.getYear());
         warAndPeace.setYear(1999);
-        System.out.println(warAndPeace);
+//        System.out.println(warAndPeace);
+        System.out.println(warAndPeace.getAuthor() + " " + warAndPeace.getBookName() + " " + warAndPeace.getYear());
 
     }
 }
